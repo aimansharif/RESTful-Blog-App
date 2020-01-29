b9a419110043c70101b0d6660c7322c483a0dc03
Sharif <msharif@ciena.com>
2020-01-29 15:14:40 -0500

    mongoose connection disabled for testing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser       = require("body-parser"),
     app              = express();
     
 //APP CONFIG
-mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
